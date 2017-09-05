1d02d3abdbbaee63151dde6505a63ef180917270
asbhadauria <31289012+asbhadauria@users.noreply.github.com>
2017-09-05 15:59:52 +0530

    Add files via upload

---
+++ A3_Q3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+		int num;
+		cout<<"Enter a number:";
+		cin>>num;
+		if(num>0)
+			cout<<"Entered number is positive";	
+		else if(num<0)
+			cout<<"Entered number is negative";	
+		else
+			cout<<"Entered number is zero";
+	return 0;
+}
