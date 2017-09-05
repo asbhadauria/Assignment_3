086bdc01221dee2851bdaae1593c3031ab1215db
asbhadauria <31289012+asbhadauria@users.noreply.github.com>
2017-09-05 16:09:02 +0530

    Add files via upload

---
+++ A3_Q5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+		
+		int num;
+		cout<<"Enter a number:";
+		cin>>num;
+		if(num%2==0)
+			cout<<"Entered number is even";	
+		else 
+			cout<<"Entered number is odd";	
+		return 0;
+}
