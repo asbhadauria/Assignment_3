3be6bda4e6ee74822edc0cb14e0bed3874a0fe5c
asbhadauria <31289012+asbhadauria@users.noreply.github.com>
2017-09-11 18:21:02 +0530

    Update A3_Q15.cpp

--- A3_Q15.cpp
+++ A3_Q15.cpp
@@ -5,11 +5,7 @@ int main() {
 	int s1,s2,s3;
 	cout<<"Enter the sides of the triangle";
 	cin>>s1>>s2>>s3;
-	if((s1+s2)>s3)
-		cout<<"Valid sides for a traingle";
-	else if((s1+s3)>s2)
-		cout<<"Valid sides for a traingle";
-	else if((s2+s3)>s1)
+	if((s1+s2)>s3 && (s1+s3)>s2 && (s2+s3)>s1)
 		cout<<"Valid sides for a traingle";
 	else 
 		cout<<"Invalid siides";
